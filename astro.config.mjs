3c4c9d4bd79672fd19d4080826e510d4a078654e
Voxi0 <alif200099@gmail.com>
2025-04-09 21:33:28 +0100

    Migrate to Tailwind
    Finally migrated from SCSS to Tailwind V4 after a couple of my friends
    kept telling me how awesome it is and seeing it's popularity on the
    internet. It's recognized as one of the best webdev stuff ever if that
    makes any sense. It's not bad at all to be honest, it's actually pretty
    nice once you get the hang of it, I'm liking it a lot so far no wonder
    it's got such a high reputation. Still got a lot of code to clean up.
    Also added some new plugins like Typography for Tailwind for beautiful
    default typography styles whatever and `postcss-nested` so I can nest
    styles just like in SCSS which I find really convenient.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Tailwind
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://voxi0.github.io/',
-	integrations: [],
+  site: 'https://voxi0.github.io/',
+  integrations: [],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
 	plugins: [
-		require('cssnano'),
+		require("cssnano"),
+		require("postcss-nested"),
 	],
 };
