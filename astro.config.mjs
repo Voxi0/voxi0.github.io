7c472c2a4f5f0e3e40485c1bb18d96a79dda8ed1
Voxi0 <alif200099@gmail.com>
2025-08-27 15:20:49 +0100

    chore: improving performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
-  site: "https://voxi0.github.io",
+  site: "https://voxi0.github.io/",
   vite: {
     plugins: [tailwindcss()]
   }
